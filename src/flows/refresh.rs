c75a61ef63976429c2517d7161ad33b7bfe9fadc
cxrvus <benito.m.rabe@gmail.com>
2024-05-21 02:49:09 +0200

    progress

--- src/flows/refresh.rs
+++ src/flows/refresh.rs
@@ -1,10 +1,19 @@
 use oxydian::prelude::*;
 
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct ViewNote {
+	pub view: String,
+	pub refreshed: Option<String>,
+	pub error: Option<String>,
+}
+
 pub const REFRESH: Flow = Flow { 
 	name: "refresh",
-	func: FreeFn(|vault| {
-		let note = Item::get(vault.path("Notes/CX.md"))?.note()?;
-		print!("{}", note.get_content());
+	func: MutatingFn(|_vault, note| {
+		let note = note.note()?;
+		let props = note.get_props::<ViewNote>();
+		let props = props.ok_or(anyhow!("Provided note is not a View Note"))?;
+		print!("{:?}\n\n\n{}", props, note.get_content());
 		Ok(())
 	})
 };
